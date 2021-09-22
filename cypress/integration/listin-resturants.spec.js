8e1922508278f25a5621167cb5667218ec9f8ae8
Eisen Uchiha <eisenuchiha@gmail.com>
2021-09-22 18:57:50 -0500

    Specify app should list restaurants

---
+++ cypress/integration/listin-resturants.spec.js
@@ -0,0 +1,21 @@
+describe('Listing Restaurants', () => {
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+
+    cy.server({force404: true});
+
+    cy.route({
+      method: 'GET',
+      url: 'https://outside-in-dev-api.herokuapp.com/75zLec3KzK7mudN1R1hrKESzxR02QdiI/restaurants',
+      response: [
+        {id: 1, name: sushiPlace},
+        {id: 2, name: pizzaPlace},
+      ],
+    });
+
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});

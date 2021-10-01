b08909e4d0b1b60da49016a51b8257ce81ca31b5
Eisen Uchiha <eisenuchiha@gmail.com>
2021-09-30 22:58:34 -0500

    Load restaurants test

---
+++ src/store/__tests__/restaurants.spec.js
@@ -0,0 +1,10 @@
+describe('restaurants', () => {
+  describe('loadRestaurants action', () => {
+    it('stores the restaurants', async () => {
+      const records = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ];
+    });
+  });
+});
